ae5ea91a65b4f0a9b8154fa7113d4abc34aacbc6
marcopisani002 <pisanimarco34@gmail.com>
2022-12-01 13:52:28 +0100

    end coding

---
+++ src/AppHero.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+        <h3><em class="em-bl">{{description}}</em></h3>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            description: "classe #79"
+        }
+    },
+}
+</script>
+<style scoped>
+.em-bl {
+    color: blue;
+
+}
+</style>
---
+++ src/AppTitle.vue
@@ -0,0 +1,30 @@
+<template lang="">
+    <div>
+        <h3>Benvenuto <em class="sp-gr">{{getUserName()}}</em> <br>______________</h3>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                firstName: "Marco",
+                lastName: "Pisani"
+            }
+
+
+        }
+
+    },
+    methods: {
+        getUserName() {
+            return this.user.firstName + " " + this.user.lastName
+        }
+    },
+}
+</script>
+<style scoped>
+.sp-gr {
+    color: green
+}
+</style>
